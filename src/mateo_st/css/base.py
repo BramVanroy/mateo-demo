afc63969aeb976e8b92260d9da59b4e895c2ac13
Bram Vanroy <Bram.Vanroy@UGent.be>
2023-04-24 16:48:14 +0200

    align translation switch btn

--- src/mateo_st/css/base.py
+++ src/mateo_st/css/base.py
@@ -7,14 +7,6 @@ def add_custom_base_style():
 a {color: #09ab3b !important;}
 a:hover {text-decoration: none;}
 
-# /* Special button alignment */
-# [data-testid="column"] + [data-testid="column"] {
-#     align-self: flex-end;
-# }
-# [data-testid="column"] + [data-testid="column"] button {
-#     padding: 7px 14px;
-# }
-
 /*  Progress bar custom color */
 .stProgress > div > div > div > div {
     background-color: #94bf89;
--- src/mateo_st/css/translation.py
+++ src/mateo_st/css/translation.py
@@ -4,11 +4,12 @@ import streamlit as st
 def add_custom_translation_style():
     st.markdown(
         """<style>
-.translations-wrapper {
-    border: 1px solid;
-    border-radius: 0.25rem;
-    padding: 0.48em 0.48em;
-    margin-bottom: 1em;
+[data-testid="column"] + [data-testid="column"] {
+    align-self: flex-end;
+    text-align: center;
+}
+[data-testid="column"] + [data-testid="column"] button {
+    padding: 7px 14px;
 }
 </style>
       """,
