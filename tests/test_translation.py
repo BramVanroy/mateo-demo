1b896fb1cb9c4fa358c7eb9ad7e15466ababa53c
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2023-10-15 11:46:40 +0200

    add test for translation textarea

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,15 +1,41 @@
-from time import sleep
-
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+
+
+def test_translation_textarea(page: Page, test_data_dir):
+    """Test textarea input on the translation page. The table with translations must contain the
+    same number of rows as there are non-empty lines in the input text.
+    """
+    translation_url = f"{pytest.mateo_st_local_url}/Translate"
+
+    page.goto(translation_url)
+    page.get_by_label("File upload?").wait_for(state="attached")
+    expect(page.get_by_label("File upload?")).not_to_be_checked()
+
+    text = (test_data_dir / "mt1.txt").read_text(encoding="utf-8")
+    num_lines = len([lstrip for l in text.splitlines() if (lstrip := l.strip())])
+
+    page.get_by_label("Sentences to translate", exact=True).fill(text)
+    page.keyboard.press("Control+Enter")
+    page.get_by_role("button", name="Translate").click()
+
+    page.get_by_text("Done translating!").wait_for()
+    results_table_el = page.locator(f'table[aria-rowcount="{num_lines + 1}"]')
+    # The table itself is hidden through some streamlit magic, so only wait until it is attached (not 'visible') in DOM
+    results_table_el.wait_for(state="attached")
+    expect(results_table_el).to_have_attribute("aria-rowcount", str(num_lines + 1))
+    expect(results_table_el).to_have_attribute("aria-colcount", "3")
 
 
 def test_translation_file_upload(page: Page, test_data_dir):
+    """Test file upload input on the translation page. The table with translations must contain the
+    same number of rows as there are non-empty lines in the input text.
+    """
     translation_url = f"{pytest.mateo_st_local_url}/Translate"
 
     page.goto(translation_url)
     page.locator("label").filter(has=page.get_by_label("File upload?", exact=True)).click()
-    assert page.get_by_label("File upload?").is_checked()
+    expect(page.get_by_label("File upload?")).to_be_checked()
 
     page.locator('[data-testid="stFileUploader"]').wait_for()
 
@@ -23,11 +49,9 @@ def test_translation_file_upload(page: Page, test_data_dir):
 
     page.get_by_role("button", name="Translate").click()
 
-    # Listen for DOM change event
-    # Check if table exists
-    # Check that the table has as many rows as the file has lines
     page.get_by_text("Done translating!").wait_for()
-    results_table_el = page.locator(".stDataFrame table")
-    sleep(3)  # sleep for a while until the table is filled
-    assert int(results_table_el.get_attribute("aria-rowcount")) == num_lines + 1
-    assert int(results_table_el.get_attribute("aria-colcount")) == 3
+    results_table_el = page.locator(f'table[aria-rowcount="{num_lines + 1}"]')
+    # The table itself is hidden through some streamlit magic, so only wait until it is attached (not 'visible') in DOM
+    results_table_el.wait_for(state="attached")
+    expect(results_table_el).to_have_attribute("aria-rowcount", str(num_lines + 1))
+    expect(results_table_el).to_have_attribute("aria-colcount", "3")
