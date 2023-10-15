5d23bddb23bea46baa89ac37b1a71cd5cbe8c28e
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2023-10-15 12:37:23 +0200

    rename test file for translate

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,10 +1,10 @@
 import pytest
 from playwright.sync_api import Page, expect
 
-translation_url = f"{pytest.mateo_st_local_url}/Translate"
-
 
 def test_swap_languages(page: Page):
+    """Test the button to change language direction"""
+    translation_url = f"{pytest.mateo_st_local_url}/Translate"
     page.goto(translation_url)
     expect(page.get_by_label("Selected English. Source language")).to_be_attached()
     expect(page.get_by_label("Selected Dutch. Target language")).to_be_attached()
@@ -17,6 +17,7 @@ def test_textarea(page: Page, test_data_dir):
     """Test textarea input on the translation page. The table with translations must contain the
     same number of rows as there are non-empty lines in the input text.
     """
+    translation_url = f"{pytest.mateo_st_local_url}/Translate"
     page.goto(translation_url)
 
     page.get_by_label("File upload?").wait_for(state="attached")
@@ -41,6 +42,7 @@ def test_file_upload(page: Page, test_data_dir):
     """Test file upload input on the translation page. The table with translations must contain the
     same number of rows as there are non-empty lines in the input text.
     """
+    translation_url = f"{pytest.mateo_st_local_url}/Translate"
     page.goto(translation_url)
 
     page.locator("label").filter(has=page.get_by_label("File upload?", exact=True)).click()
